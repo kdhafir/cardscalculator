d6375dc610aef79078f81fad7fd1a2992dc73874
kdhafir <kdhafir@ymail.com>
2017-10-19 21:06:30 +0300

    Add files via upload

---
+++ www/js/code.js
@@ -0,0 +1,24 @@
+
+
+//Local Storage test
+if (typeof(Storage) !== "undefined") {
+    // Code for localStorage/sessionStorage.
+} else {
+    // Sorry! No Web Storage support..
+}
+
+
+
+// Formating Numbers by adding commas
+function addCommas(nStr)
+{
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+}
